41f18a6926babf10218a1097b9d5ff9a194042ba
atika-beep <raazhra14@gmail.com>
2024-11-04 20:18:22 +0700

    TUGAS BESAR1

---
+++ Tugas Besar1/script.js
@@ -0,0 +1,8 @@
+function orderCake(cakeName) {
+    alert('Anda telah memesan ' + cakeName + '. Terima kasih!');
+}
+
+function submitForm(event) {
+    event.preventDefault();
+    alert('Pesan Anda telah dikirim! Terima kasih.');
+}
